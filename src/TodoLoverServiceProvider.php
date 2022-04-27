760a441effc16ea8b218cb0e4d7bf485c23e7569
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-04-27 06:42:57 +0430

    feat: package provider with routes and config

---
+++ src/TodoLoverServiceProvider.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Geeksesi\TodoLover;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class TodoLoverServiceProvider extends ServiceProvider
+{
+ /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Route::middlewareGroup("todo_lover_api", config("todo_lover.api_middleware", []));
+        Route::middlewareGroup("todo_lover_web", config("todo_lover.web_middleware", []));
+        $this->registerRoutes();
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . "/../config/todo_lover.php", "todo_lover");
+    }
+
+    /**
+     * Register the package routes.
+     *
+     * @return void
+     */
+    private function registerRoutes()
+    {
+        Route::group(
+            [
+                "prefix" => "api/" . config("todo_lover.path"),
+                "middleware" => "todo_lover_api",
+            ],
+            function () {
+                $this->loadRoutesFrom(__DIR__ . "/../config/routes/api.php");
+            }
+        );
+        Route::group(
+            [
+                "prefix" => config("todo_lover.path"),
+                "middleware" => "todo_lover_web",
+            ],
+            function () {
+                $this->loadRoutesFrom(__DIR__ . "/../config/routes/web.php");
+            }
+        );
+    }
+}
