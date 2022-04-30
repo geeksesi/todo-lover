339b8484f1fde4157dc514110baefd92acec8f5b
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-04-30 07:24:18 +0430

    feat: task status enum. old enum

---
+++ src/TaskStatusEnum.php
@@ -0,0 +1,28 @@
+<?php
+
+class TaskStatusEnum
+{
+    public const OPEN = 1;
+    public const TODO = 2;
+    public const INPROGRESS = 3;
+    public const DONE = 4;
+    public const CLOSE = 5;
+
+    public const ALL = [self::OPEN, self::TODO, self::INPROGRESS, self::DONE, self::CLOSE];
+
+    private const STRINGIFY = [
+        self::OPEN => "open",
+        self::TODO => "todo",
+        self::INPROGRESS => "inprogress",
+        self::DONE => "done",
+        self::CLOSE => "close",
+    ];
+
+    public static function toString(int $enum)
+    {
+        if (!in_array($enum, self::ALL)) {
+            return null;
+        }
+        return self::STRINGIFY[$enum];
+    }
+}
