d88acb8a9bcc681bce78815045d82062e2404e30
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-05-01 15:59:16 +0430

    fix: tasks relation

--- src/Models/Label.php
+++ src/Models/Label.php
@@ -7,11 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Label extends Model
 {
-
     protected $fillable = ["title"];
 
     public function tasks()
     {
-        return $this->belongsToMany(Task::class, "lable_task");
+        return $this->belongsToMany(Task::class, "label_task");
     }
 }
