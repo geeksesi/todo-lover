28c5fb6087c96c1a391a5496eb88781f8f9eec65
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-05-01 15:59:33 +0430

    fix: user should be authenticatable

--- src/Models/User.php
+++ src/Models/User.php
@@ -4,8 +4,9 @@ namespace Geeksesi\TodoLover\Models;
 
 use Geeksesi\TodoLover\HasTaskTrait;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
     use HasTaskTrait;
 }
