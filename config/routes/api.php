722b10886b146d85fb7a55d865e41b0cbb3443d4
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-05-01 16:03:30 +0430

    feat: routes

--- config/routes/api.php
+++ config/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
+use Geeksesi\TodoLover\Http\Controllers\LabelController;
+use Geeksesi\TodoLover\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::post("bot", function(){
-
-});
+Route::apiResource("tasks", TaskController::class)->except("destroy");
+Route::apiResource("labels", LabelController::class)->only("index");
