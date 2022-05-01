2c8d259fce9a61844225e4b9e0e77aa1c0f0db1e
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-05-01 16:03:51 +0430

    fix: label migration

--- database/migrations/2022_04_29_091049_create_label_task_table.php
+++ database/migrations/2022_04_29_091049_create_label_task_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLableTaskTable extends Migration
+class CreateLabelTaskTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateLableTaskTable extends Migration
      */
     public function up()
     {
-        Schema::create("lable_task", function (Blueprint $table) {
+        Schema::create("label_task", function (Blueprint $table) {
             $table->id();
             $table
-                ->foreignId("lable_id")
-                ->constant("lables")
+                ->foreignId("label_id")
+                ->constant("labels")
                 ->onDelete("CASCADE");
             $table
                 ->foreignId("task_id")
@@ -34,6 +34,6 @@ class CreateLableTaskTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists("lable_task");
+        Schema::dropIfExists("label_task");
     }
 }
