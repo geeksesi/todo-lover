4f23354ef2c5d2841d1386741520da417c0fd2c9
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-05-01 15:58:28 +0430

    feat: task service + create task logic

---
+++ src/Services/TaskService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Geeksesi\TodoLover\Services;
+
+use Geeksesi\TodoLover\Models\Task;
+use Geeksesi\TodoLover\Models\User;
+use Geeksesi\TodoLover\TaskStatusEnum;
+use Illuminate\Support\Facades\Auth;
+
+class TaskService
+{
+    public function create(array $data, \OwnerModel $owner): Task
+    {
+        if (!empty($data["labels"])) {
+            $labels = $this->handleLabels($data["labels"]);
+        }
+        $task = new Task();
+        $task->title = $data["title"];
+        $task->description = $data["description"];
+        $task->status = TaskStatusEnum::OPEN;
+
+        $owner->tasks()->save($task);
+        return $task;
+    }
+
+    public function update(Task $task, array $data): Task
+    {
+        # code...
+    }
+
+    private function handleLabels(array $labels): array
+    {
+        return [];
+    }
+}
