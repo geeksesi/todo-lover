783ff20f509087233ca8b7fb38fbb34a51f74ab1
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-05-01 15:57:33 +0430

    test: basic task creation

--- tests/Feature/TasksTest.php
+++ tests/Feature/TasksTest.php
@@ -4,25 +4,25 @@ namespace Geeksesi\TodoLover\Tests;
 
 use Geeksesi\TodoLover\Models\Label;
 use Geeksesi\TodoLover\Models\Task;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Str;
 
-class TasksTest extends TestCase {
-    
+class TasksTest extends TestCase
+{
     public function test_create_task()
     {
         $owner = $this->authentication();
 
-        $lable = factory(Label::class)->create();
-        $task_mock = factory(Task::class);
-        
-        $res = $this->postJson(route('todo_lover.task.create'), [
-            "label"=> $lable->id,
-            "title"=> $task_mock->title,
-            "description"=> $task_mock->description,
+        $res = $this->postJson("api/todo_lover/tasks", [
+            // "label" => ["سلام"],
+            "title" => Str::random(10),
+            "description" => Str::random(100),
         ]);
         $res->assertSuccessful();
+        $res->dump();
+
         $res->assertJsonStructure([
-            "data"=> ["lable", "title", "description", "owner"]
+            "data" => ["labels", "title", "description"],
         ]);
-        $res->assertJson([]);
     }
 }
