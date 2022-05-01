ab14bae6419318464e18f08d17f5eaa6bf56b30b
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-05-01 16:01:30 +0430

    feat: controller

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -2,8 +2,14 @@
 
 namespace Geeksesi\TodoLover\Http\Controllers;
 
-use App\Http\Controllers\Controller as ControllersController;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as ControllersController;
 
 class Controller extends ControllersController
 {
+    use AuthorizesRequests;
+    use DispatchesJobs; 
+    use ValidatesRequests;
 }
