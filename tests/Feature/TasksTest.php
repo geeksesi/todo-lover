77742d84ecf4893d2b629756e9f39cd7a58d9392
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-05-02 12:09:35 +0430

    test: show task test

--- tests/Feature/TasksTest.php
+++ tests/Feature/TasksTest.php
@@ -4,8 +4,11 @@ namespace Geeksesi\TodoLover\Tests;
 
 use Geeksesi\TodoLover\Models\Label;
 use Geeksesi\TodoLover\Models\Task;
+use Geeksesi\TodoLover\Models\User;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Str;
+
+use function PHPUnit\Framework\assertEquals;
 
 class TasksTest extends TestCase
 {
@@ -19,10 +22,43 @@ class TasksTest extends TestCase
             "description" => Str::random(100),
         ]);
         $res->assertSuccessful();
-        $res->dump();
+        // $res->dump();
 
         $res->assertJsonStructure([
             "data" => ["labels", "title", "description"],
         ]);
+    }
+
+    public function test_show_logedin_user_task()
+    {
+        $owner = $this->authentication();
+        $task = factory(Task::class)->make();
+        $owner->tasks()->save($task);
+
+        $res = $this->get("api/todo_lover/tasks/" . $task->id);
+        $res->assertSuccessful();
+        // $res->dump();
+
+        $res->assertJsonStructure([
+            "data" => ["labels", "title", "description"],
+        ]);
+
+        assertEquals(
+            $res->original->owner->id,
+            $owner->id,
+            "owner is not equal test: " . $owner->id . " actual: " . $res->original->owner->id
+        );
+    }
+
+    public function test_cant_show_others_task()
+    {
+        $owner = $this->authentication();
+        $other_owner = factory(\OwnerModel::class)->create();
+        $task = factory(Task::class)->make();
+        $other_owner->tasks()->save($task);
+
+        $res = $this->get("api/todo_lover/tasks/" . $task->id);
+        $res->assertForbidden();
+        // $res->dump();
     }
 }
