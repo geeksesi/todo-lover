b957973255c049eb38ce96f9fa84066e6aac59e9
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2022-05-02 12:16:22 +0430

    feat: label index authorize

--- src/Http/Requests/Label/IndexRequest.php
+++ src/Http/Requests/Label/IndexRequest.php
@@ -3,6 +3,7 @@
 namespace Geeksesi\TodoLover\Http\Requests\Label;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class IndexRequest extends FormRequest
 {
@@ -13,7 +14,7 @@ class IndexRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return Auth::check();
     }
 
     /**
@@ -23,8 +24,6 @@ class IndexRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-        ];
+        return [];
     }
-
 }
